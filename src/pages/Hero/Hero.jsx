60e8c2d6f161592652ab152dc39601e947aba417
cuongnv0112@mkjgame.net <cuongnv0112@mkjgame.net>
2026-03-05 12:33:38 +0700

    full

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -52,7 +52,7 @@ const profile = {
     name: 'Nguyen Viet Cuong',
     title: 'Mobile Developer',
     skills: [
-        'Flutter', 'Android', 'Java', 'ReactJS',
+        'Flutter', 'Android', React Native, 'Java', 'ReactJS',
     ],
     hardWorker: true,
     quickLearner: true,
